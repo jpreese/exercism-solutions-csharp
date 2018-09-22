a8c77cd0ef518d5d1cdc2def79a0b96ea2df0dcb
John Reese <jpreese@mtu.edu>
2018-09-22 19:34:00 -0400

    Solve Sum of Multiples

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        return Enumerable.Range(1, max - 1)
+            .Where(i => multiples.Any(m => i % m == 0))
+            .Sum();
+    }
+}
